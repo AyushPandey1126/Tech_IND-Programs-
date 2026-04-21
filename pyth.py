a493a193d0b572f0226c35cda14365d67ebd8f1e
Ayush Pandey <ayushpandey88408@gmail.com>
2026-04-21 18:34:45 +0530

    2nd day working on loops basics ( Questions )

--- pyth.py
+++ pyth.py
@@ -26,7 +26,7 @@ while i >= 1 :
     print(i)
     i-=1
 for i in range(10,0,-1) :
-    print(i)'''
+    print(i)
 # Q3 : Print even numbers from 1 - 50 using both while loop and dor loop .
 
 i = 1
@@ -34,4 +34,105 @@ while i<=50:
     if i%2 == 0 : 
         print(i)
         i+=1
+for i in range(1,51):
+    if i%2 == 0 :
+        print(i)
+
+# Q4 : Print odd numbers from 1 - 50 using both while loop and for loop .
+
+i = 1 
+
+while i <= 50 :
+    if i%2 != 0 :
+        print(i)
+    i+=1
+# Q5 : Find the sum of all n numbers from 1 to n using both while loop and for loop .
+
+n = int(input("enter a number n to find the sumof all numbers from 1 - n :"))
+sum = 0 
+i = 1 
+while i <= n :
+    sum +=i
+    i+=1
+    print(f"the sum of all numbers from 1 - {n} is {sum}")
+
+n = int(input("enter a number n to find the sumof all numbers from 1 - n :"))
+sum = 0 
+for i in range(1,n+1):
+    sum +=i 
+    print(f"the sum of {n} is {sum}")
+# Q6 : Print the multiplication table of a given number n using both while loop and for loop .
+
+n = int(input("enter a number n to find its multiplication table :"))
+i = 1 
+while i <= 10 :
+    print(f"{n} * {i} = {n*i}")
+    i+=1
+
+n = int(input("enetr the number n to find its multipplication table :"))
+for i in range(1,11):
+    print(f"{n}*{i} = {n*i}")
+# Q7 : Count how many numbers between 1 - 100 are divisible by 3 , use loop to solve . 
+
+i = 1 
+count = 0 
+while i <= 100 :
+    if i%3 == 0 :
+        count +=1 
+    i+=1
+print(f"the count of numbers between 1 - 100 that are divisible by 3 is {count}")
+
+# Q8 : Find the factorial of a number n using loops . 
+n = int(input("enter a number n to find its factorial :"))
+factorial = 1 
+i = 1 
+while i <=n :
+    factorial *=i
+    print(f"the factorial of {n} is {factorial}")
+    i+=1 
+
+n = int(input("enter a number n to find its factorial :"))   
+
+factorial = 1 
+
+for i in range(1,n+1):
+    factorial *=i
+    print(f"the factorial of {n} ia {factorial}")
+
+# Q9 : Reverse a number using loops . 
+
+n = int(input("enter a n number n to reverse it :"))
+i = 1 
+reverse = 0 
+while n > 0 : 
+    digit = n%10
+    reverse = reverse * 10 + digit 
+    n = n//10 
+    print(f"the reverse {n} is {reverse}")
+    i+=1 '''
+n = int(input("enter a number n to reverse it well : "))
+
+reverse = 0 
+for i in range(len(str(n))):
+    digit = n%10 
+    reverse = reverse * 10 + digit 
+    n = n//10
+    print(f"the reverse of {n} is {reverse}")
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
 
